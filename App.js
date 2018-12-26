bd8da270289fef7c89c2cdc6dac80ab166cc10b2
James Kung <kong1191@gmail.com>
2018-12-26 17:01:39 +0800

    Rename environment variable name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const linebot = require('linebot');
 const bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSecret: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+    channelAccessToken: process.env.CHANNEL_TOKEN
 });
 
 const app = express(); //creates an Express application
