7a83901aea2006d3347065d35e0c84bfbc61f9da
shawnkaku <shawnkuo67@gmail.com>
2019-01-03 11:40:53 +0800

    add a unit test sample
    - move rest API routing implementation to app_router.js
    - add 'npm test' command to trigger mocha

--- App.js
+++ App.js
@@ -1,36 +1,4 @@
-const express = require('express'); //require express module
-const linebot = require('linebot');
-
-const bot = linebot({
-    channelId: process.env.CHANNEL_ID,
-    channelSecret: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_TOKEN
-});
-
-const app = express(); //creates an Express application
-
-const linebotParser = bot.parser();
-
-// Redirect linebot webhook url to linebot parser
-app.post('/linewebhook', linebotParser);
-
-bot.on('message', function (event) {
-    event.reply(event.message.text).then(function (data) {
-        console.log('Success', data);
-    }).catch(function (error) {
-        console.log('Error', error);
-    });
-});
-
-//Express API --------- App.get('path', callback function);
-//routes HTTP GET requests to the specified path with the specified callback functions
-app.get('/', function (request, response) {
-    response.send("response from node service");
-});
-
-app.post('/ajax', function (request, response) {
-    response.send("response by ajax");
-});
+var app = require('./app_router.js')
 
 //The 'process' object is a global that provides information about, and control over, the current Node.js process.
 //As a global, it is always available to Node.js applications without using require().
@@ -41,5 +9,4 @@ const port = process.env.PORT || 3000;
 
 //binds and listens for connections on the specified host and port. This method is identical to Node’s http.Server.listen().
 app.listen(port);
-
 console.log(`MERN Stack listening on ${port}`);
---
+++ app_router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const linebot = require('linebot');
+var app = module.exports = express();
+
+const bot = linebot({
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_TOKEN
+});
+
+const linebotParser = bot.parser();
+
+// Redirect linebot webhook url to linebot parser
+app.post('/linewebhook', linebotParser);
+
+bot.on('message', function (event) {
+    event.reply(event.message.text).then(function (data) {
+        console.log('Success', data);
+    }).catch(function (error) {
+        console.log('Error', error);
+    });
+});
+
+//Express API --------- App.get('path', callback function);
+//routes HTTP GET requests to the specified path with the specified callback functions
+app.get('/', function (request, response) {
+    response.json({ message: 'response from node service!' });
+});
+
+app.post('/ajax', function (request, response) {
+    response.send("response by ajax");
+});
